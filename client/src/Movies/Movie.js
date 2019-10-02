d78d27f162d7a258217595ebd6a0a911112ae2b0
Neilson <Brennan.Neilson@henryschein.com>
2019-10-02 11:14:12 -0600

    Movie pages working

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -3,34 +3,37 @@ import axios from 'axios';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState({});
- 
+
   useEffect(() => {
-    const id = 1;
+    const { id } = props.match.params;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
+          console.log(response.data);
           setMovie(response.data);
+          console.log(response.data);
         })
         .catch(error => {
           console.error(error);
         });
 
   },[]);
-  
+
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
   //   const addToSavedList = props.addToSavedList;
   //   addToSavedList(movie)
   // }
 
-  if (!movie) {
+  const { title, director, metascore, stars } = movie;
+
+  if (!movie || !stars) {
     return <div>Loading movie information...</div>;
   }
-
-  const { title, director, metascore, stars } = movie;
+  
   return (
     <div className="save-wrapper">
       <div className="movie-card">
